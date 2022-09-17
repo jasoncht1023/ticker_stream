e10d72cb137a63fac9f3100a3ee7632947d38c00
Jason Chan <jasoncht1023@gmail.com>
2022-09-17 11:09:07 +0800

    added ticker stream functions

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-import codeitsuisse.routes.square
+import codeitsuisse.routes.to_cumulative
+import to_cumulative_delayed
 
 
 
--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -9,12 +9,55 @@ logger = logging.getLogger(__name__)
 
 @app.route('/square', methods=['POST'])
 def evaluate():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    def to_cumulative(stream: list):
+    output = str(stream[0][:5])
+    dataList = list()
+    for record in stream:
+        recordList = list(record[6:].split(','))
+        recordList[2] = str(round(float(recordList[1]) * float(recordList[2]), 1))
+        recordStr = ''
+        for i in recordList:
+            recordStr = recordStr + ','
+            recordStr = recordStr + i
+        dataList.append(recordStr)
+    dataList.sort()
+    for record in dataList:
+        output = output + record
+    outputList = list()
+    outputList.append(output)
+    return(outputList)
+
+def to_cumulative_delayed(stream: list, quantity_block: int):
+    outputList = list()
+    streamSorted = sorted(stream, key = lambda x: (x[6], x))
+    ticker = streamSorted[0][6]
+    cumQuan = 0
+    cumNotion = 0
+    for record in streamSorted:
+        recordList = list(record.split(','))
+        if (recordList[1] != ticker):
+            ticker = recordList[1]
+            cumQuan = 0
+            cumNotion = 0
+        while (recordList[2] != '0'):
+            cumNotion += float(recordList[3])
+            cumQuan += 1
+            recordList[2] = str(int(recordList[2]) - 1)
+            if (cumQuan % quantity_block == 0):
+                found = False
+                index = 0
+                while (found == False and index < len(outputList)):
+                    if (outputList[index][0] == recordList[0]):
+                        found = True
+                    index += 1
+                if (found == True):
+                    outputList[index].extend([',', ticker, ',', str(cumQuan), ',', str(cumNotion)])
+                else:
+                    outputList.append([recordList[0], ',', ticker, ',', str(cumQuan), ',', str(cumNotion)])
+    outputList.sort()
+    for i in range(len(outputList)):
+        outputList[i] = ''.join(outputList[i])
+    return outputList
 
 
 
